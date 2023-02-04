3bfcabe45a25d9fe69f8e6942c377016abd3857e
luisfe82 <92070822+luisfe82@users.noreply.github.com>
2023-02-04 15:06:27 -0600

    agrega archivos
    agrega archivos

---
+++ com/alura/hotel/HotelMain.java
@@ -0,0 +1,10 @@
+package com.alura.hotel;
+
+import com.alura.hotel.views.MenuPrincipal;
+
+public class HotelMain {
+	public static void main(String[] args) {
+		MenuPrincipal ventanaPrincipal = new MenuPrincipal();
+		ventanaPrincipal.setVisible(true);
+	}  
+}
